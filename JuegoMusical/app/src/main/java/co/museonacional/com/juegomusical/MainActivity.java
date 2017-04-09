9218dafd145e233e5f758d66bfcd78d6a20bf91b
McDrak <caguayo93@hotmail.com>
2017-04-09 01:58:06 -0500

    Proyecto Museo Nacional -> Cancelado

---
+++ JuegoMusical/app/src/main/java/co/museonacional/com/juegomusical/MainActivity.java
@@ -0,0 +1,15 @@
+package co.museonacional.com.juegomusical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ JuegoMusical/app/src/main/java/co/museonacional/com/juegomusical/Pantalla01.java
@@ -0,0 +1,13 @@
+package co.museonacional.com.juegomusical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla01);
+    }
+}
